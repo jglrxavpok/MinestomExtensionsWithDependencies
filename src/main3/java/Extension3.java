0a907a8175e274bd243ef9ee1820cfd8887c927d
jglrxavpok <jglrxavpok@gmail.com>
2020-11-06 16:03:08 +0100

    Let users change extension jar after unload, then load it again

--- src/main3/java/Extension3.java
+++ src/main3/java/Extension3.java
@@ -1,8 +1,17 @@
 import dependentextension.DependentExtension;
 import extension2.Extension2;
+import net.minestom.server.MinecraftServer;
+import net.minestom.server.command.builder.Command;
 import net.minestom.server.extensions.Extension;
 
 public class Extension3 extends Extension {
+
+    private Command myCommand = new Command("extension3.command") {
+        {
+            setDefaultExecutor((source, args) -> source.sendMessage("Hello from extension3, after being reloaded! "+Extension3.class.hashCode()));
+        }
+    };
+
     @Override
     public void initialize() {
         System.out.println("[Extension3] Extension1 was initialized at (in nanoseconds): "+ DependentExtension.initializationTime);
@@ -10,10 +19,13 @@ public class Extension3 extends Extension {
         System.out.println("[Extension3] Extension1 class hashCode: "+ DependentExtension.class.hashCode());
         System.out.println("[Extension3] Extension2 has class: "+ Extension2.class);
         System.out.println("[Extension3] Extension2 class hashCode: "+ Extension2.class.hashCode());
+
+        MinecraftServer.getCommandManager().register(myCommand);
     }
 
     @Override
     public void terminate() {
-
+        System.out.println("UNLOADING COMMAND");
+        MinecraftServer.getCommandManager().unregister(myCommand);
     }
 }
